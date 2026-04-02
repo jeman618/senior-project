09790c5341316561067aa73da57af8cd60af60bd
jeman618 <jcisne23@calpoly.edu>
2026-04-02 13:06:01 -0700

    Working on getting server to work on school wifi

--- packages/backend/access_db.js
+++ packages/backend/access_db.js
@@ -3,7 +3,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_URL,
   ssl: { rejectUnauthorized: false },
 });
 
